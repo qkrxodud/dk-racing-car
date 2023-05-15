8270ab060e5c7f01a90cde1415b09e20607123b2
mia <mia.ahn@kakaocorp.com>
2023-05-15 22:48:07 +0900

    test : 문자열 split 테스트코드 작성 추가

--- src/test/java/com/study/racing/StringTest.java
+++ src/test/java/com/study/racing/StringTest.java
@@ -1,19 +1,18 @@
 package com.study.racing;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Nested;
 
 @DisplayName("\uD83D\uDE80String 클래스에 대한 학습 테스트 - 1단계")
-class CarTest {
+class StringTest {
 	String regex = ",";
 
-	@Nested
 	@Test
 	@DisplayName("요구사항 1-1")
-	public void case_1() {
+	public void case_1_1() {
 		String testStr = "1,2";
 		String[] strings = testStr.split(regex);
 
@@ -23,11 +22,32 @@ class CarTest {
 
 	@Test
 	@DisplayName("요구사항 1-2")
-	public void case_2() {
+	public void case_1_2() {
 		String testStr = "1";
 		String[] strings = testStr.split(regex);
 
 		// containsExactly 순서와 값이 정확히 일치
 		assertThat(strings).containsExactly("1");
+	}
+
+	@Test
+	@DisplayName("요구사항 2")
+	public void case_2() {
+		String testStr = "(1,2)";
+		// 앞과 뒤 괄호 제거
+		testStr = testStr.substring(1, testStr.length()-1);
+
+		// 값 일치
+		assertThat(testStr).isEqualTo("1,2");
+	}
+
+	@Test
+	@DisplayName("요구사항 3")
+	public void case_3() {
+		String testStr = "abc";
+		int idx = testStr.length();
+
+		assertThatThrownBy(() -> System.out.println(testStr.charAt(idx)))
+				.isInstanceOf(StringIndexOutOfBoundsException.class);
 	}
 }
