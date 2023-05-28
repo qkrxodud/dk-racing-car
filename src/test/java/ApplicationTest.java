1b3ecdf69e1d2ba093ee6bb593be3794068ca2cf
don-jh1025 <don.jh1025@kakaocorp.com>
2023-05-28 17:15:13 +0900

    기본 세팅 테스트

---
+++ src/test/java/ApplicationTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest {
+
+    @Test
+    @DisplayName("테스트")
+    public void test() {
+        System.out.println("test");
+    }
+}
